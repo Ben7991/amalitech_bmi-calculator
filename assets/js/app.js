4e6566718ca7dc931dafde02d5f9da16956631cd
Bernard Teye <datssosh@gmail.com>
2023-09-18 11:03:33 -0700

    index.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,53 @@ for (let i = 0; i < rbMeasurementUnits.length; i++) {
         }
     });
 }
+class BMI {
+    constructor(weight, height) {
+        this._weight = 0; // kg
+        this._height = 0;
+        this._weight = weight;
+        this._height = height;
+    }
+}
+class MetricsBMI extends BMI {
+    constructor(weight, height) {
+        super(weight, height);
+        this.Height = height; // override the height value to meters
+    }
+    /**
+     * receiving height in cm and converting to meters
+     * due to the formula for calculating in meters
+     */
+    set Height(height) {
+        this._height = height / 100;
+    }
+    calculateBMI() {
+        return this._weight / Math.pow(this._height, 2);
+    }
+}
+class ImperialBMI extends BMI {
+    constructor(weight, height) {
+        super(weight, height);
+        this.Weight = weight;
+        this.Height = height;
+    }
+    /**
+     * as per the imperial calculation of bmi
+     * weight is to be in pounds and due to the fact of receiving
+     * weight in stone. converting to pounds only
+     */
+    set Weight(weight) {
+        this._weight = weight * 14;
+    }
+    /**
+     * as per the imperial calculation of bmi
+     * height is to be in inches and due the receiving value in
+     * ft. Convert the value to inches only
+     */
+    set Height(height) {
+        this._height = height * 12;
+    }
+    calculateBMI() {
+        return (this._weight / Math.pow(this._height, 2)) * 703;
+    }
+}
--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -14,3 +14,65 @@ for (let i = 0; i < rbMeasurementUnits.length; i++) {
         }
     });
 }
+
+abstract class BMI {
+    protected _weight: number = 0;     // kg
+    protected _height: number = 0;  
+
+    public constructor(weight: number, height: number) {
+        this._weight = weight;
+        this._height = height;
+    }
+
+    public abstract calculateBMI(): number;
+}
+
+class MetricsBMI extends BMI {
+    public constructor(weight: number, height: number) {
+        super(weight, height);
+        this.Height = height;   // override the height value to meters
+    }
+
+    /**
+     * receiving height in cm and converting to meters
+     * due to the formula for calculating in meters
+     */
+    public set Height(height: number) {   
+        this._height = height / 100;
+    }
+
+    public override calculateBMI(): number {
+        return this._weight / Math.pow(this._height, 2);
+    }
+}
+
+
+class ImperialBMI extends BMI {
+    public constructor(weight: number, height: number) {
+        super(weight, height);
+        this.Weight = weight;
+        this.Height = height;
+    }
+
+    /**
+     * as per the imperial calculation of bmi
+     * weight is to be in pounds and due to the fact of receiving 
+     * weight in stone. converting to pounds only
+     */
+    public set Weight(weight: number) {
+        this._weight = weight * 14;
+    }
+
+    /**
+     * as per the imperial calculation of bmi
+     * height is to be in inches and due the receiving value in 
+     * ft. Convert the value to inches only
+     */
+    public set Height(height: number) {
+        this._height = height * 12;
+    }
+
+    public override calculateBMI(): number { 
+        return ( this._weight / Math.pow(this._height, 2) ) * 703; 
+    }
+}
