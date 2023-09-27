33e3c823388a97ebeca5e91b151248328e842dbe
Bernard Teye <datssosh@gmail.com>
2023-09-27 14:29:08 -0700

    re-configure all event handlers of imperial component class

--- assets/ts/components/imperial-component.ts
+++ assets/ts/components/imperial-component.ts
@@ -1,49 +1,71 @@
 import { ImperialBMI } from "../models/imperial-bmi.js";
+import { ImperialConvertor } from "../utility/ImperialConvertor.js";
 
 export class ImperialComponent {
-    private feat: HTMLInputElement;
+    private feet: HTMLInputElement;
     private inches: HTMLInputElement;
     private stone: HTMLInputElement;
     private pounds: HTMLInputElement;
+    private bmi: ImperialBMI;
 
     public constructor() {
-        this.feat = <HTMLInputElement>document.getElementById("feat");
+        this.feet = <HTMLInputElement>document.getElementById("feat");
         this.inches = <HTMLInputElement>document.getElementById("inches");
         this.stone = <HTMLInputElement>document.getElementById("stone");
         this.pounds = <HTMLInputElement>document.getElementById("pounds");
 
         this.configure();
+        this.bmi = new ImperialBMI(0,0);
     }
 
     private configure() {
-        this.feat.addEventListener("change", () => {
+        this.feet.addEventListener("change", () => {
+            let parsedFeet = parseFloat(this.feet.value);
+            if (isNaN(parsedFeet)) 
+                return;
+
+            this.inches.value = ImperialConvertor.convertFeetToInchesInHeight(parsedFeet).toString();
             this.calculate();
         });
 
         this.inches.addEventListener("change", () => {
+            let parsedInches = parseFloat(this.inches.value);
+            if (isNaN(parsedInches)) 
+                return;
+
+            this.feet.value = ImperialConvertor.convertInchesToFeetInHeight(parsedInches).toString();
             this.calculate();
         });
 
         this.stone.addEventListener("change", () => {
+            let parsedStone = parseFloat(this.stone.value);
+            if (isNaN(parsedStone)) 
+                return;
+
+            this.pounds.value = ImperialConvertor.convertStoneToPoundsInWeight(parsedStone).toString();
             this.calculate();
         });
 
         this.pounds.addEventListener("change", () => {
+            let parsedPounds = parseFloat(this.pounds.value);
+            if (isNaN(parsedPounds)) 
+                return;
+
+            this.stone.value = ImperialConvertor.convertPoundsToStoneInWeight(parsedPounds).toString();
             this.calculate();
         });
     }
 
     private calculate() {
-        if (this.feat.value === "" || this.inches.value === "" || this.pounds.value === "" || this.stone.value === "")
+        if (this.feet.value === "" || this.inches.value === "" || this.pounds.value === "" || this.stone.value === "")
             return;
 
-        let height = parseFloat(this.feat.value) + parseFloat("0." + this.inches.value);
-        let weight = parseFloat(this.stone.value) + parseFloat("0." + this.pounds.value);
-        
-        let bmi = new ImperialBMI(weight, height);
-        document.querySelector(".header__result-answer-heading")!.textContent = bmi.calculateBMI().toFixed(2);
+        this.bmi.Weight = parseFloat(this.pounds.value);
+        this.bmi.Height = parseFloat(this.inches.value);
+
+        document.querySelector(".header__result-answer-heading")!.textContent = this.bmi.calculateBMI().toFixed(2);
         document.querySelector(".header__result-info")!.classList.remove("d-none");
         document.querySelector(".header__result-welcome")!.classList.add("d-none");
-        document.querySelector(".header__result-description-info")!.innerHTML = bmi.Description;
+        document.querySelector(".header__result-description-info")!.innerHTML = this.bmi.Description;
     }
 }
