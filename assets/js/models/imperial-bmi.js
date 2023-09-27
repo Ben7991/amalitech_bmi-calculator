dc5959b206a40d9dc737b856fac53c3fec67660b
Bernard Teye <datssosh@gmail.com>
2023-09-27 14:26:21 -0700

    remove both weight and height setter accessor methods

--- assets/js/models/imperial-bmi.js
+++ assets/js/models/imperial-bmi.js
@@ -2,24 +2,6 @@ import { BMI } from "./bmi.js";
 export class ImperialBMI extends BMI {
     constructor(weight, height) {
         super(weight, height);
-        this.Weight = weight;
-        this.Height = height;
-    }
-    /**
-     * as per the imperial calculation of bmi
-     * weight is to be in pounds and due to the fact of receiving
-     * weight in stone. converting to pounds only
-     */
-    set Weight(weight) {
-        this._weight = weight * 14;
-    }
-    /**
-     * as per the imperial calculation of bmi
-     * height is to be in inches and due to the receiving value in
-     * ft. Convert the value to inches only
-     */
-    set Height(height) {
-        this._height = height * 12;
     }
     calculateBMI() {
         return (this._weight / Math.pow(this._height, 2)) * 703;
--- assets/ts/models/imperial-bmi.ts
+++ assets/ts/models/imperial-bmi.ts
@@ -3,26 +3,6 @@ import { BMI } from "./bmi.js";
 export class ImperialBMI extends BMI {
     public constructor(weight: number, height: number) {
         super(weight, height);
-        this.Weight = weight;
-        this.Height = height;
-    }
-
-    /**
-     * as per the imperial calculation of bmi
-     * weight is to be in pounds and due to the fact of receiving 
-     * weight in stone. converting to pounds only
-     */
-    public set Weight(weight: number) {
-        this._weight = weight * 14;
-    }
-
-    /**
-     * as per the imperial calculation of bmi
-     * height is to be in inches and due to the receiving value in 
-     * ft. Convert the value to inches only
-     */
-    public set Height(height: number) {
-        this._height = height * 12;
     }
 
     public override calculateBMI(): number { 
